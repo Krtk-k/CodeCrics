e36995af465f4259d13aafd63bcb0ef8a553b013
Krtk-k <kartikkpandey05@gmail.com>
2025-03-11 14:07:26 +0530

    added authentication function for login and created signIn.txt

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
@@ -14,7 +15,7 @@ class Path {
         cin >> mainDirPath;
     }
     ofstream signUp() {
-        ofstream signup(mainDirPath + "/userLog.txt", ios::app);
+        ofstream signup(mainDirPath + "/userSign.txt", ios::app);
         return (signup);
     }
     ifstream logIn() {
@@ -28,6 +29,35 @@ class Path {
 string line1 = "\t\tWelcome to CodeCrics", line2 = "\t\t1->Login, 2->Signup";
 string inpErr = "Invalid input";
 
+
+void authenticate(string &name, string &pass, Path &directory) {
+    // This function checks for existing user
+    string fname = NULL, fpass = NULL;
+    ifstream l_file = directory.logIn();
+    char ch;
+    bool matchFound = false;
+    while(1) {
+        while((l_file.get(ch)) && (ch != '\0')) {
+            fname.push_back(ch);
+        }
+        l_file.seekg(ios::cur + 1);
+        while(l_file.get(ch) && (ch != '\n')) {
+            fpass.push_back(ch);
+        }
+
+        if((name == fname) && (pass == fpass)) {
+            cout << endl << "User Found, Welcome";
+            matchFound = true;
+            break;
+        }
+    }
+
+    if(matchFound == false) {
+        cout << endl << "You are not an existing user, signup first";
+        exit(0);
+    }
+}
+
 int main() {
     cout << endl << line1;
     Path mdp;
@@ -38,6 +68,7 @@ int main() {
 
     cin.ignore(); 
     string usernameL, pwdL, usernameS, pwdS;
+    ofstream file;
     switch(userIn) {
         case 1: // Login
             cout << endl << "Username: ";
@@ -45,6 +76,7 @@ int main() {
             getline(cin, usernameL);
             cout << endl << "Password: ";
             getline(cin, pwdL);
+            authenticate(usernameL, pwdL, mdp);
             break;
 
         case 2: // Signup
